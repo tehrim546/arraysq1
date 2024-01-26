0935a23af2b848a80ddc9a97632e926de2aa1d40
= <=>
2024-01-26 06:55:38 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+//Declare an array of fruits and print each one using a loop. You can use a for loop to iterate through the array.
+const fruits: string[]= ["apple","banana","orange","grapes","mango"];
+//using a fo loop to iterate through the array and print each fruit
+for(let i=0; i <fruits.length;i++){
+    console.log(fruits[i]);
+}
